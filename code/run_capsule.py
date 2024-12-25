1f1ed5d998b58907237b33ee6768d362bb9e4121
arielleleon <54869022+arielleleon@users.noreply.github.com>
2024-12-24 19:47:12 -0800

    9 serialize dataprocess object (#10)
    * Add aind-log-utils to Docker
    
    * Add logging and clean up metadata grabs.
    
    * Add and serialize DataProcess object.
    
    * Added datasets.json; edited Dockerfile
    
    * Edited run_capsule.py, Dockerfile
    
    * Added resources.json; edited run_capsule.py
    
    * Edited run_capsule.py
    
    * Edited run_capsule.py
    
    * Deleted datasets.json; edited run_capsule.py
    
    * Remove ununsed imports.
    
    * Sort imports.
    
    * Fix bugs in run_capsule module.'
    
    * Added datasets.json; edited run_capsule.py
    
    * Lint run_capsule module.
    
    * Edited run_capsule.py
    
    * Edited metadata.yml
    
    * Declare output_dir post instance check.
    
    * Add unique identifier to data proc object.
    
    * Remove testing name.
    
    * Edited run_capsule.py

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -5,24 +5,25 @@ import os
 from datetime import datetime as dt
 from multiprocessing.pool import Pool
 from pathlib import Path
-from typing import Union
+from typing import Tuple, Union
 
 import h5py
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
-from aind_data_schema.core.processing import DataProcess, PipelineProcess, Processing, ProcessName
+from aind_data_schema.core.processing import DataProcess, ProcessName
+from aind_log_utils.log import setup_logging
 from oasis.functions import deconvolve
 from oasis.oasis_methods import oasisAR1, oasisAR1_f32, oasisAR2
 
 
-def write_output_metadata(
+def write_data_process(
     metadata: dict,
-    process_json_dir: str,
-    process_name: str,
     input_fp: Union[str, Path],
     output_fp: Union[str, Path],
-    start_date_time: dt,
+    unique_id: str,
+    start_time: dt,
+    end_time: dt,
 ) -> None:
     """Writes output metadata to plane processing.json
 
@@ -30,35 +31,33 @@ def write_output_metadata(
     ----------
     metadata: dict
         parameters from suite2p motion correction
-    input_fp: str
-        path to data input
+    raw_movie: str
+        path to raw movies
     output_fp: str
-        path to data output
+        path to motion corrected movies
+    unique_id: str
+        unique identifier for the processing
+    start_time: dt
+        start time of processing
+    end_time: dt
+        end time of processing
     """
-    with open(Path(process_json_dir) / "processing.json", "r") as f:
-        proc_data = json.load(f)
-    processing = Processing(
-        processing_pipeline=PipelineProcess(
-            processor_full_name="Multplane Ophys Processing Pipeline",
-            pipeline_url=os.getenv("PIPELINE_URL", ""),
-            pipeline_version=os.getenv("PIPELINE_VERSION", ""),
-            data_processes=[
-                DataProcess(
-                    name=process_name,
-                    software_version=os.getenv("VERSION", ""),
-                    start_date_time=start_date_time,
-                    end_date_time=dt.now(),
-                    input_location=str(input_fp),
-                    output_location=str(output_fp),
-                    code_url=os.getenv("REPO_URL"),
-                    parameters=metadata,
-                )
-            ],
-        )
+    data_proc = DataProcess(
+        name=ProcessName.FLUORESCENCE_EVENT_DETECTION,
+        software_version=os.getenv("VERSION", ""),
+        start_date_time=start_time.isoformat(),
+        end_date_time=end_time.isoformat(),
+        input_location=str(input_fp),
+        output_location=str(output_fp),
+        code_url=(os.getenv("REPO_URL", "")),
+        parameters=metadata,
     )
-    prev_processing = Processing(**proc_data)
-    prev_processing.processing_pipeline.data_processes.append(processing.processing_pipeline.data_processes[0])
-    prev_processing.write_standard_file(output_directory=Path(output_fp).parent)
+    if isinstance(output_fp, str):
+        output_dir = Path(output_fp).parent
+    else:
+        output_dir = output_fp.parent
+    with open(output_dir / f"{unique_id}_oasis_events_data_process.json", "w") as f:
+        json.dump(json.loads(data_proc.model_dump_json()), f, indent=4)
 
 
 def make_output_directory(output_dir: Path, experiment_id: str) -> Path:
@@ -84,7 +83,9 @@ def make_output_directory(output_dir: Path, experiment_id: str) -> Path:
     return output_dir
 
 
-def plot_trace_and_events_png(trace, ca, spike, timestamps, roi_id, tau, plots_path, show_fig=False) -> None:
+def plot_trace_and_events_png(
+    trace, ca, spike, timestamps, roi_id, tau, plots_path, show_fig=False
+) -> None:
     sns.set_context("talk")
     fig, ax = plt.subplots(2, 1, figsize=(20, 5), sharex=True)
     ax[0].plot(timestamps, 100 * trace, color="C0", label=r"raw $\Delta$F/F")
@@ -104,10 +105,37 @@ def plot_trace_and_events_png(trace, ca, spike, timestamps, roi_id, tau, plots_p
         plt.close(fig)
 
 
+def get_metadata(input_dir: Path, meta_type: str) -> dict:
+    """Extracts metadata from processing and subject json files
+
+    Parameters
+    ----------
+    input_dir: Path
+        input directory
+    meta_type: str
+        type of metadata to extract
+
+    Returns
+    -------
+    metadata: dict
+        metadata
+    """
+    input_fp = next(input_dir.rglob(f"{meta_type}"), "")
+    if not input_fp:
+        raise FileNotFoundError(f"No {meta_type} file found in {input_dir}")
+    with open(input_fp, "r") as f:
+        metadata = json.load(f)
+    return metadata
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--input-dir", type=str, default="../data/", help="Input directory")
-    parser.add_argument("-o", "--output-dir", type=str, default="../results/", help="Output directory")
+    parser.add_argument(
+        "-i", "--input-dir", type=str, default="../data/", help="Input directory"
+    )
+    parser.add_argument(
+        "-o", "--output-dir", type=str, default="../results/", help="Output directory"
+    )
     parser.add_argument(
         "--estimate_parameters",
         type=bool,
@@ -144,7 +172,12 @@ if __name__ == "__main__":
         help="Fluorescence baseline value. If no value is given, "
         "then b is optimized if estimate_parameters==True else 0.",
     )
-    parser.add_argument("--b_nonneg", type=bool, default=True, help="Enforce strictly non-negative baseline if True")
+    parser.add_argument(
+        "--b_nonneg",
+        type=bool,
+        default=True,
+        help="Enforce strictly non-negative baseline if True",
+    )
     parser.add_argument(
         "--sn",
         type=float,
@@ -152,7 +185,12 @@ if __name__ == "__main__":
         help="Standard deviation of the noise distribution. If no value is given, "
         "then sn is estimated from the data based on power spectral density.",
     )
-    parser.add_argument("--penalty", type=int, default=1, help="Sparsity penalty. 1: min |s|_1  0: min |s|_0")
+    parser.add_argument(
+        "--penalty",
+        type=int,
+        default=1,
+        help="Sparsity penalty. 1: min |s|_1  0: min |s|_0",
+    )
     parser.add_argument(
         "--lam",
         type=float,
@@ -162,12 +200,14 @@ if __name__ == "__main__":
         "under L1 penalty if estimate_parameters==True else 0",
     )
     parser.add_argument(
-        "--s_min", type=float, default=0, help="Minimal non-zero activity within each bin (minimal 'spike size')."
+        "--s_min",
+        type=float,
+        default=0,
+        help="Minimal non-zero activity within each bin (minimal 'spike size').",
     )
     parser.add_argument("--no_qc", action="store_true", help="Skip QC plots.")
     args = parser.parse_args()
     params = vars(args)
-
     start_time = dt.now()
     output_dir = Path(args.output_dir).resolve()
     input_dir = Path(args.input_dir).resolve()
@@ -175,13 +215,18 @@ if __name__ == "__main__":
     experiment_id = dff_dir.parent.name
     dff_fp = next(dff_dir.glob("*dff.h5"))
     output_dir = make_output_directory(output_dir, experiment_id)
-    process_json_fp = dff_dir / "processing.json"
-    with open(process_json_fp, "r") as f:
-        process_json = json.load(f)
-    for data_process in process_json["processing_pipeline"]["data_processes"]:
-        if data_process["name"] == "Video motion correction":
-            frame_rate = data_process["parameters"]["movie_frame_rate_hz"]
-
+    session_data = get_metadata(input_dir, "session.json")
+    try:
+        frame_rate = float(session_data["data_streams"][0]["ophys_fovs"][0]["frame_rate"])
+    except (KeyError, IndexError):
+        raise ("Frame rate not located in the session.json")
+    subject_data = get_metadata(input_dir, "subject.json")
+    subject_id = subject_data.get("subject_id", "")
+    data_description_data = get_metadata(input_dir, "data_description.json")
+    name = data_description_data.get("name", "")
+    setup_logging(
+        "aind-ophys-oasis-event-detection", mouse_id=subject_id, session_name=name
+    )
     # convert time constants to parameters of the auto-regressive (AR) process
     if args.tau is None or args.tau_rise is None:  # automatically estimate tau
         if args.tau_rise == 0:  # negligible rise time -> AR1
@@ -192,7 +237,10 @@ if __name__ == "__main__":
         if args.tau_rise == 0:  # negligible rise time -> AR1
             params["g"] = (np.exp(-1 / (args.tau * frame_rate)),)
         else:  # AR2
-            d, r = (np.exp(-1 / (args.tau * frame_rate)), np.exp(-1 / (args.tau_rise * frame_rate)))
+            d, r = (
+                np.exp(-1 / (args.tau * frame_rate)),
+                np.exp(-1 / (args.tau_rise * frame_rate)),
+            )
             params["g"] = (d + r, -d * r)
 
     if not args.estimate_parameters:
@@ -219,7 +267,9 @@ if __name__ == "__main__":
             return (c, s, np.nan, np.nan, np.nan) if args.estimate_parameters else (c, s)
         else:
             if args.estimate_parameters:
-                relevant_params = {k: params[k] for k in ["g", "sn", "b", "b_nonneg", "penalty"]}
+                relevant_params = {
+                    k: params[k] for k in ["g", "sn", "b", "b_nonneg", "penalty"]
+                }
                 relevant_params["optimize_g"] = params["optimize_tau"]
                 return deconvolve(t, **relevant_params)
             elif args.tau_rise == 0:
@@ -227,7 +277,13 @@ if __name__ == "__main__":
                     t - params["b"], args.g[0], s_min=args.s_min, lam=args.lam
                 )
             else:
-                return oasisAR2(t.astype(float) - params["b"], args.g[0], args.g[1], s_min=args.s_min, lam=args.lam)
+                return oasisAR2(
+                    t.astype(float) - params["b"],
+                    args.g[0],
+                    args.g[1],
+                    s_min=args.s_min,
+                    lam=args.lam,
+                )
 
     try:
         print(f"Performing Event Detection for {experiment_id}")
@@ -245,7 +301,9 @@ if __name__ == "__main__":
             res = pool.map(_deconv, traces)
             calcium, spikes = [np.array([r[i] for r in res], dtype="f4") for i in (0, 1)]
             if args.estimate_parameters:
-                b_hat, g_hat, lam_hat = [np.array([r[i] for r in res], dtype="f4") for i in (2, 3, 4)]
+                b_hat, g_hat, lam_hat = [
+                    np.array([r[i] for r in res], dtype="f4") for i in (2, 3, 4)
+                ]
                 # convert parameters of the auto-regressive (AR) process to time constants
                 if g_hat.ndim == 1:  # AR1
                     tau_hat = -1 / np.log(g_hat) / frame_rate
@@ -282,7 +340,8 @@ if __name__ == "__main__":
                     plot_trace_and_events_png,
                     zip(
                         traces,
-                        calcium + (b_hat[:, None] if args.estimate_parameters else params["b"]),
+                        calcium
+                        + (b_hat[:, None] if args.estimate_parameters else params["b"]),
                         spikes,
                         [timestamps] * N,
                         range(N),
@@ -297,11 +356,11 @@ if __name__ == "__main__":
         logging.error(f"FAILED: {experiment_id}")
         raise e
 
-    write_output_metadata(
+    write_data_process(
         params,
-        dff_dir,
-        ProcessName.FLUORESCENCE_EVENT_DETECTION,
         dff_fp,
         oasis_h5,
+        experiment_id,
         start_time,
+        end_time=dt.now(),
     )
